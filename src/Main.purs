8a8d3c0bce54cf3407e41b9d3d3e96cbed296b87
Jason Kuhrt <jasonkuhrt@me.com>
2016-03-11 14:36:53 -0500

    Create an index.html file to bootstrap the app. Modify Main produce a viewable effect.

--- src/Main.purs
+++ src/Main.purs
@@ -3,16 +3,18 @@ module Main where
 import Prelude (Unit, bind)
 import Data.Maybe (Maybe(Just))
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console
 import Graphics.Drawing
 import Graphics.Canvas (getCanvasElementById, getContext2D, Canvas)
 
 
 
-main :: forall a. Partial =>  Eff (canvas :: Canvas | a) Unit
+-- main :: forall a. Partial =>  Eff (canvas :: Canvas | a) Unit
 main = do
-  Just canvas <- getCanvasElementById "canvas"
-  ctx <- getContext2D canvas
-  render ctx shape
-
-  where
-  shape = filled (fillColor black) (circle 1.0 1.0 1.0)
+  log "Hello World."
+  -- Just canvas <- getCanvasElementById "canvas"
+  -- ctx <- getContext2D canvas
+  -- render ctx shape
+  --
+  -- where
+  -- shape = filled (fillColor black) (circle 1.0 1.0 1.0)
