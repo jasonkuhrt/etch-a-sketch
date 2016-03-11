1f9cb96d8224826fb8c012006d40c533ee272425
Jason Kuhrt <jasonkuhrt@me.com>
2016-03-11 14:59:49 -0500

    Render a graphic into the app.

--- src/Main.purs
+++ src/Main.purs
@@ -1,20 +1,17 @@
 module Main where
 
-import Prelude (Unit, bind)
+import Prelude
 import Data.Maybe (Maybe(Just))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console
 import Graphics.Drawing
-import Graphics.Canvas (getCanvasElementById, getContext2D, Canvas)
-
+import Graphics.Canvas
 
 
 -- main :: forall a. Partial =>  Eff (canvas :: Canvas | a) Unit
 main = do
-  log "Hello World."
-  -- Just canvas <- getCanvasElementById "canvas"
-  -- ctx <- getContext2D canvas
-  -- render ctx shape
-  --
-  -- where
-  -- shape = filled (fillColor black) (circle 1.0 1.0 1.0)
+  Just canvas <- getCanvasElementById "canvas"
+  ctx <- getContext2D canvas
+  render ctx shape
+
+  where
+  shape = filled (fillColor (hsl 30.0 1.0 0.5)) (circle 40.0 40.0 40.0)
